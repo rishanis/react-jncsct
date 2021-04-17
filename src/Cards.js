e7af179d06966a0c5a546de319b31cefa4896f22
rishanis <rishanis@outlook.com>
2021-04-17 13:41:17 +0400

    1 file modified

--- src/Cards.js
+++ src/Cards.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Cards = (props) => {
   return (
     <div style={{backgroundColor: 'teal'}}>
-      <img alt='picture' />
+      <img alt='picture' src={`https://robohash.org/${ props.id }`}/>
       <h3>{ props.name }</h3>
       <p>{ props.email }</p>  
     </div>
